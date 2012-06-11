5e9b56d0c9e1607d3cf560d59f030a43ec50b0dc
Juan Osuna <juan@brownbagconsulting.com>
2012-06-11 21:16:43 +0200

    Fixed bug where many-to-one popup button missing

--- expressui-core/src/main/java/com/expressui/core/view/field/SelectField.java
+++ expressui-core/src/main/java/com/expressui/core/view/field/SelectField.java
@@ -260,11 +260,6 @@ public class SelectField extends CustomField {
     }
 
     @Override
-    public boolean isReadOnly() {
-        return field.isReadOnly();
-    }
-
-    @Override
     public void addValidator(Validator validator) {
         field.addValidator(validator);
     }
